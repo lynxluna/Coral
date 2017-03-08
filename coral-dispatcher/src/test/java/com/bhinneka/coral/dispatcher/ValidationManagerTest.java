bcf0b0e800d40d82cebdff9027181a96bf6857f1
Didiet <lynxluna@gmail.com>
2017-03-08 22:59:24 +0700

    Adding ValidationManager

--- coral-dispatcher/src/test/java/com/bhinneka/coral/dispatcher/ValidationManagerTest.java
+++ coral-dispatcher/src/test/java/com/bhinneka/coral/dispatcher/ValidationManagerTest.java
@@ -1,7 +1,10 @@
 package com.bhinneka.coral.dispatcher;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.*;
 
+import com.bhinneka.coral.dispatcher.exceptions.ValidationException;
+import org.assertj.core.api.ThrowableAssert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -27,5 +30,23 @@ public class ValidationManagerTest {
 
     verify(mockValidator, times(1))
         .validate(eq(new Person("", 0)), eq(new CreatePerson("Didiet", 22)));
+  }
+
+  @Test
+  public void testValidationException() {
+    when(mockValidator.validate(eq(new Person("", 0)), any(ChangePersonName.class)))
+        .thenThrow(new ValidationException(new ChangePersonName("Didiet"), new Person("", 0)));
+
+    final ValidationManager<Person> validationManager = ValidationManager
+        .<Person>newBuilder()
+        .addValidator(ChangePersonName.class, mockValidator)
+        .build();
+
+    assertThatThrownBy(new ThrowableAssert.ThrowingCallable() {
+      @Override
+      public void call() throws Throwable {
+        validationManager.validate(new Person("", 0), new ChangePersonName("Didiet"));
+      }
+    }).isInstanceOf(ValidationException.class);
   }
 }
